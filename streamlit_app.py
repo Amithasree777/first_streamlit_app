6d676ca73b5a1fdcd7187392a8f085e9cf496f2f
Amithasree777 <112540583+Amithasree777@users.noreply.github.com>
2022-09-01 11:33:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,10 @@ streamlit.text('Hard-Boiled Free- Range EGG')
 
 streamlit.title('My Moms New Healthy Dinner')
 streamlit.header('Breakfast favorites')
+streamlit.text('🥣Omega 3&Blueberry oat Meal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free- Range EGG')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
